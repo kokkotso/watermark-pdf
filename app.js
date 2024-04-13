25086ee89074bc2f6558542f10306e56ac8bc374
Kokko Tso <kokkotso@gmail.com>
2024-04-13 16:41:05 -0400

    feature: call PDF rest API & return watermarked file

--- app.js
+++ app.js
@@ -3,7 +3,40 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    const axios = require('axios');
+    const FormData = require('form-data');
+    const fs = require('fs');
+    const path = require('path');
+
+    const pdfRestKey = process.env.PDFRESTKEY;
+
+    const pdfPath = path.join(__dirname, 'assets', 'dummypdf.pdf');
+
+    console.log(pdfPath);
+    
+    const data = new FormData();
+    data.append('file', fs.createReadStream(pdfPath));
+    data.append("watermark_text", "This file was generated for testing.");
+    
+    const config = {
+        method: 'post',
+        maxBodyLength: Infinity,
+        url: 'https://api.pdfrest.com/watermarked-pdf',
+        headers: {
+            'Api-Key': 'afc79ba2-29a7-49cb-b088-3d8d9792b351',
+            ...data.getHeaders()
+        },
+        data: data
+    };
+    
+    return axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+      res.redirect(response.data.outputUrl);
+    })
+    .catch(function (error) {
+      console.log(error); 
+    });
 });
 
 app.listen(port, () => {
